4df660edb0e63f8f418fa02b9e697d4c2113bed5
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-11-18 12:57:59 +0500

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = (operation, error) => {
+    if (operation === "increment" && count < 20) {
+      setCount(count + 1);
+    } else if (operation === "decrement" && count > 0) {
+      setCount(count - 1);
+    } else {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => handleCount("increment", "Cannot increment beyond 20")}>
+          increase {count}
+        </button>
+        <button onClick={() => handleCount("decrement", "Cannot decrement below 0")}>
+          decrease {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
